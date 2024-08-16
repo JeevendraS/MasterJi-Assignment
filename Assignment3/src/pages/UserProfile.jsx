d530f683281403de1fe50efc54ac8708975d4ed3
Jeevendra Singh <Jeevendrasingh1999@gmail.com>
2024-08-16 19:23:42 +0530

    added functionality

--- Assignment3/src/pages/UserProfile.jsx
+++ Assignment3/src/pages/UserProfile.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faRotateRight, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 function UserProfile() {
-  return (
+  const [user, setUser] = React.useState({})
+  const [userDob, setUserDob] = React.useState({})
+
+  const randomUser = async()=> {
+    await fetch("https://api.freeapi.app/api/v1/public/randomusers/user/random")
+                        .then(res => res.json())
+                        .then(data=> {
+                          setUser(data)
+                          console.log(data.data.name.title)
+                          const dob = data.data.dob.date
+                          const dateObj = new Date(dob)
+                          const options = {day: 'numeric', month: 'long', year: 'numeric'}
+                          const formatDate = dateObj.toLocaleString('en-US',options)
+                          setUserDob({dateOfB: formatDate})
+                        })
+
+  }
+  useEffect(()=>{
+    randomUser()
+  },[])
+
+  console.log(userDob)
+
+
+  if(user.data) return (
     <div className='bg-gray-300 w-screen h-screen flex items-center justify-center '>
       <div className='bg-white w-[28vw] h-[95vh] rounded-[15px] absolute flex items-center justify-center'>
         <div  className='w-[97%] h-[98%] bg-[#B6B3F3] rounded-[10px] relative flex items-center justify-center'>
           <div className=' w-[95%] h-[97%]'>
             <div className=' h-[5%] text-black flex flex-row justify-between mx-2 '>
-              <FontAwesomeIcon className='my-auto' icon={faArrowLeft} />
+              <FontAwesomeIcon className='my-auto active:translate-x-1' icon={faArrowLeft} />
               <h3 className='my-auto font-serif font-normal text-[16px]'>Profile Overview</h3>
-              <FontAwesomeIcon className='my-auto' icon={faRotateRight} />
+              <FontAwesomeIcon className='my-auto active:rotate-45' icon={faRotateRight} />
             </div>
             <div className='w-full h-[35vh] flex items-center justify-center flex-col'>
-              <p className='text-[8px] text-white rounded-md right-[8rem] top-[4rem] absolute h-3 w-5 text-center bg-black'>Mr</p>
-              <img src="https://s3-alpha-sig.figma.com/img/0584/17a5/4f205aba496f239ced6e754fd1ca71f7?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nJCvuJ9S3S14Dlwj69182PAUTeL0aK8U8Aqf~LznENvZSu4HgEJf5rRF9GWOnLsy2MO0Q7Nm8H86sozUaAzn3kjdOvta5yv0vrqhnlHFsHjjwa20T9YSMNWFliw~3cwT9GyfUmuQFec~zQR-pcGv5qXPyKg-vHA6n8qiJWjs0HrGC6UK1nyx3p9oWKP73-jNvpWV4nnav8GxnP2d~EQTXIGLUUBJUzyewMSLYKlV~JqeVN2jA17vwASfYEVUODpSZaG54YCBqabHv0xL9-HUKg8iYDzubcSd44oY6baeBgWAOOQ7sH3-W~v4RdPeLBdKC1p0Ugl4APwt47g66My9ig__" 
+              <p className='text-[8px] text-white rounded-md right-[8rem] top-[4rem] absolute h-3 w-5 text-center bg-black'>{user.data.name.title}</p>
+              <img src={`${user.data.picture.large}`} 
               alt="User Photo" 
               className='rounded-full w-[100px] h-[100px] mb-5' />
-              <h1 className='text-[24px] font-serif font-normal tracking-normal'>Tom Garza</h1>
-              <h4 className='text-[12px] font-sans'>beautifullcoel355</h4>
+              <h1 className='text-[24px] font-serif font-normal tracking-normal'>{user.data.name.first+" "+user.data.name.last}</h1>
+              <h4 className='text-[12px] font-sans'>{user.data.login.username}</h4>
             </div>
             <div className='w-full flex items-center gap-2 justify-center h-10 border-gray-400 border-t border-b'>
               <div className='w-1/2 flex items-center gap-1 justify-center pl-24'>
@@ -35,25 +59,25 @@ function UserProfile() {
               <div>
                 <div>
                   <h4 className='font-sans font-normal text-[10px]'>City</h4>
-                  <h2 className='font-serif text-[20px] font-normal'>Mildura</h2>
+                  <h2 className='font-serif text-[20px] font-normal'>{user.data.location.city}</h2>
                 </div>
                 <div>
                   <h4 className='font-sans font-normal text-[10px]'>Date of birth</h4>
-                  <h2 className='font-serif text-[20px] font-normal'>09 December, 1982</h2>
+                  <h2 className='font-serif text-[20px] font-normal'>{userDob.dateOfB}</h2>
                 </div>
                 <div>
                   <h4 className='font-sans font-normal text-[10px]'>Time zone</h4>
-                  <h2 className='font-serif text-[20px] font-normal'>-10:00 (Hawaii)</h2>
+                  <h2 className='font-serif text-[20px] font-normal'>{`${user.data.location.timezone.offset}`}</h2>
                 </div>
               </div>
               <div>
                 <div>
                   <h4 className='font-sans font-normal text-[10px]'>Nationality</h4>
-                  <h2 className='font-serif text-[20px] font-normal'>AU</h2>
+                  <h2 className='font-serif text-[20px] font-normal'>{user.data.location.country}</h2>
                 </div>
                 <div>
                   <h4 className='font-sans font-normal text-[10px]'>Phone No.</h4>
-                  <h2 className='font-serif text-[20px] font-normal '>123-456-789</h2>
+                  <h2 className='font-serif text-[20px] font-normal '>{user.data.phone}</h2>
                 </div>
                 <div>
                   <h4 className='font-sans font-normal text-[10px]'>Regestered since</h4>
