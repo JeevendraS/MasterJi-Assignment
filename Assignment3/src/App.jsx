42930ee6dae4f63602d547ead61c6e022ff836e4
Jeevendra Singh <Jeevendrasingh1999@gmail.com>
2024-08-24 23:58:35 +0530

    update

--- Assignment3/src/App.jsx
+++ Assignment3/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -7,7 +8,14 @@ function App() {
 
   return (
     <>
-      <Outlet/>
+      <div>
+        <Outlet/>
+        <div className='absolute left-0 top-64 flex flex-col p-4 text-white'>
+            <button type="button" Link="/" className='bg-gray-500 p-2 rounded-lg m-2 shadow-xl active:scale-95'>User Profile</button>
+            <button type="button" Link="/random-jokes" className='bg-gray-500 p-2 rounded-lg m-2 shadow-xl active:scale-95'>Jokes</button>
+            <button type="button" Link="/cats-listing" className='bg-gray-500 p-2 rounded-lg m-2 shadow-xl active:scale-95'>Cats Listing</button>
+      </div>
+      </div>
     </>
   )
 }
